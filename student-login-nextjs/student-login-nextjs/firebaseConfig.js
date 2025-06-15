96484cc34b55c78cd0ed630f59505c0b33dc1b63
Agent-Virus <115211286+code-with-virus@users.noreply.github.com>
2025-06-15 15:01:32 +0530

    Update firebaseConfig.js

--- student-login-nextjs/student-login-nextjs/firebaseConfig.js
+++ student-login-nextjs/student-login-nextjs/firebaseConfig.js
@@ -1,14 +1,25 @@
 // firebaseConfig.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBCHG-UftrS9UirbxFUeSP688JXut5Fw_s",
+  authDomain: "huhe-e75b5.firebaseapp.com",
+  projectId: "huhe-e75b5",
+  storageBucket: "huhe-e75b5.firebasestorage.app",
+  messagingSenderId: "320267974170",
+  appId: "1:320267974170:web:faf02a5e3f7d0a7169c3a3",
+  measurementId: "G-J13PX0T19D"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 };
 
 const app = initializeApp(firebaseConfig);
